a7c93fbfa23bca2f5f2898896667345f523b8f8d
Vavana-Banana <vladimir9vladimir9@gmail.com>
2021-01-24 17:09:01 +0300

    jet test

--- src/eu/senla/task1/numbers.java
+++ src/eu/senla/task1/numbers.java
@@ -1,4 +1,13 @@
 package eu.senla.task1;
 
 public class numbers {
+    private byte aByte;
+
+    public byte getaByte() {
+        return aByte;
+    }
+
+    public void setaByte(byte aByte) {
+        this.aByte = aByte;
+    }
 }
